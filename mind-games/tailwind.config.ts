c3f272aea6e7db66dae7c3d0da857094176d5869
muhammed-gumus <mr.silver.mg@gmail.com>
2024-02-05 16:23:39 +0300

    feat(ming-games): set tailwind custom screen sizes

--- mind-games/tailwind.config.ts
+++ mind-games/tailwind.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "320px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -17,4 +24,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
